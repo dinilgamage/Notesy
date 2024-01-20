50ebef90df36ac661adaae10b279555d6b5a94c0
Dinil <dinilgamage5@gmail.com>
2024-01-21 02:37:42 +0530

    google authentication implemented

--- app/src/main/java/com/abc/notesy/Login.java
+++ app/src/main/java/com/abc/notesy/Login.java
@@ -6,25 +6,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login extends AppCompatActivity {
 
-    private EditText mloginemail,mloginpassword;
-    private Button mloginbutton;
-    private TextView mgotoregister,mforgotpassword;
+    private EditText mloginemail, mloginpassword;
+    private Button mloginbutton, mgooglesigninbutton;
+    private TextView mgotoregister, mforgotpassword;
 
     private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,25 +44,40 @@ public class Login extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
-        mloginemail=findViewById(R.id.emailInput);
-        mloginpassword=findViewById(R.id.passwordInput);
-        mloginbutton=findViewById(R.id.loginButton);
-        mgotoregister=findViewById(R.id.registerPrompt);
-        mforgotpassword=findViewById(R.id.forgotPassword);
+        mloginemail = findViewById(R.id.emailInput);
+        mloginpassword = findViewById(R.id.passwordInput);
+        mloginbutton = findViewById(R.id.loginButton);
+        mgotoregister = findViewById(R.id.registerPrompt);
+        mforgotpassword = findViewById(R.id.forgotPassword);
+        mgooglesigninbutton = findViewById(R.id.googleSignInButton);
 
-        firebaseAuth=FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
-        if(firebaseUser!=null){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mgooglesigninbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+        if (firebaseUser != null) {
             finish();
-            Intent intent=new Intent(Login.this,MainActivity.class);
+            Intent intent = new Intent(Login.this, MainActivity.class);
             startActivity(intent);
         }
 
         mgotoregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,Register.class);
+                Intent intent = new Intent(Login.this, Register.class);
                 startActivity(intent);
             }
         });
@@ -61,7 +85,7 @@ public class Login extends AppCompatActivity {
         mforgotpassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,ForgotPassword.class);
+                Intent intent = new Intent(Login.this, ForgotPassword.class);
                 startActivity(intent);
             }
         });
@@ -69,40 +93,83 @@ public class Login extends AppCompatActivity {
         mloginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email=mloginemail.getText().toString().trim();
-                String password=mloginpassword.getText().toString().trim();
-                if(email.isEmpty() || password.isEmpty()){
-                    Toast.makeText(getApplicationContext(),"All fields are required", Toast.LENGTH_SHORT).show();
-
-                }else{
-                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                String email = mloginemail.getText().toString().trim();
+                String password = mloginpassword.getText().toString().trim();
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+                } else {
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 checkEmailVerification();
-                            }
-                            else{
-                                Toast.makeText(getApplicationContext(),"Account doesn't exist",Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Account doesn't exist", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
                 }
             }
         });
-
     }
 
-    private void checkEmailVerification(){
-            FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
-            if(firebaseUser.isEmailVerified()==true){
-                Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_SHORT).show();
-                finish();
-                Intent intent=new Intent(Login.this,MainActivity.class);
-                startActivity(intent);
-            }else{
-                Toast.makeText(getApplicationContext(),"Verify your email",Toast.LENGTH_SHORT).show();
-                firebaseAuth.signOut();
-            }
-        }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account.getIdToken());
+        } catch (ApiException e) {
+            Toast.makeText(Login.this, "Google sign in failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(Login.this, "Firebase Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            // User is signed in
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // User is signed out
+            Toast.makeText(Login.this, "Please sign in to continue.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void checkEmailVerification() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser.isEmailVerified()) {
+            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+            finish();
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), "Verify your email", Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
+    }
 }
--- app/src/main/java/com/abc/notesy/Register.java
+++ app/src/main/java/com/abc/notesy/Register.java
@@ -1,5 +1,6 @@
 package com.abc.notesy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,16 +12,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Register extends AppCompatActivity {
 
     private EditText mregisteremail,mregisterpassword,mregisterconfirmpassword;
-    private Button mregisterbutton;
+    private Button mregisterbutton, mgooglesigninbutton;
     private TextView mgotologin;
 
     private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +50,24 @@ public class Register extends AppCompatActivity {
         mregisterconfirmpassword=findViewById(R.id.confirmpasswordInput);
         mregisterbutton=findViewById(R.id.registerButton);
         mgotologin=findViewById(R.id.loginPrompt);
+        mgooglesigninbutton = findViewById(R.id.googleSignInButton);
+
 
         firebaseAuth=FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mgooglesigninbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
 
         mgotologin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,6 +122,52 @@ public class Register extends AppCompatActivity {
         }
         else {
             Toast.makeText(getApplicationContext(), "No authenticated user found", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account.getIdToken());
+        } catch (ApiException e) {
+            Toast.makeText(Register.this, "Google sign in failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(Register.this, "Firebase Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            // User is signed in
+            Intent intent = new Intent(Register.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // User is signed out
+            Toast.makeText(Register.this, "Please sign in to continue.", Toast.LENGTH_SHORT).show();
         }
     }
 }
