088a35f24276fb164f18aee15fe3cd2c968df9dc
Dinil <dinilgamage5@gmail.com>
2024-01-20 21:22:36 +0530

    minor changes

--- app/src/main/java/com/abc/notesy/ForgotPassword.java
+++ app/src/main/java/com/abc/notesy/ForgotPassword.java
@@ -9,6 +9,9 @@ public class ForgotPassword extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_forgot_password);
     }
 }
