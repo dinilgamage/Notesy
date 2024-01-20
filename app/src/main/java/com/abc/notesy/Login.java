0307d6ef169ecbcbb29177826a2acd0d00fcf7e9
Dinil <dinilgamage5@gmail.com>
2024-01-20 18:40:41 +0530

    Login register ui completed

---
+++ app/src/main/java/com/abc/notesy/Login.java
@@ -0,0 +1,19 @@
+package com.abc.notesy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login);
+    }
+}
