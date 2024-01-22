05ff845491ee5340c33e9cc87b0a09a111f4a254
Dinil <dinilgamage5@gmail.com>
2024-01-22 09:08:42 +0530

    nav bar implemented

--- app/src/main/java/com/abc/notesy/MainActivity.java
+++ app/src/main/java/com/abc/notesy/MainActivity.java
@@ -1,14 +1,43 @@
 package com.abc.notesy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.abc.notesy.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+                return true;
+            } else if (item.getItemId() == R.id.add) {
+                replaceFragment(new AddFragment());
+                return true;
+            } else if (item.getItemId() == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+                return true;
+            }
+            return false;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
